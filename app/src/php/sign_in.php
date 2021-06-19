ac5abcb5d72bddac87bfa002f7461b379a40bf2d
= <felixfreidman@gmail.com>
2021-06-19 13:05:19 +0800

    1:05PM Commit FFIX

--- app/src/php/sign_in.php
+++ app/src/php/sign_in.php
@@ -9,13 +9,10 @@
 	$password = $_POST['pass-log'];
 	$login = filter_var(trim($login), FILTER_SANITIZE_STRING); 
 	$pass = filter_var(trim($password), FILTER_SANITIZE_STRING); 
-	$sql1 = $dbo->prepare("SELECT * FROM `users` WHERE `login` = ? LIMIT 1;");
-	$sql2 = $dbo->prepare("SELECT * FROM `users` WHERE `password` = ? LIMIT 1;");
-	$sql1->execute([$login]);
-	$sql2->execute([$password]);
-	$result1 = $sql1->fetch();
-	$result2 = $sql2->fetch();
-	if(($login == $result1['login']) && ($password == $result2['password'])) {
+	$sql = $dbo->prepare("SELECT * FROM `users` WHERE `login` = ? AND `password` = ?;");
+	$sql->execute([$login, $password]);
+	$result = $sql->fetch();
+	if(($login == $result['login']) && ($password == $result['password'])) {
 		header("Location: https://calendarapp.hostfl.ru/app/src/pages/calendar.php");
 		exit();
 	}else {
